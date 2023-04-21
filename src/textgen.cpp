48b2fc3a444e95c0dd1bf470828f9a11f8f0d914
KseniaKarepanova <126484812+KseniaKarepanova@users.noreply.github.com>
2023-04-21 09:17:29 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,43 +1,41 @@
+// Copyright 2021 GHA Test Team
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <map>
 #include <string>
 #include <deque>
-#include <cstdlib>
 #include <ctime>
+#include <stdlib.h>
 #include"textgen.h"
-using namespace std;
 
-const int NPREF = 2; // количество слов в префиксе
-const int MAXGEN = 1000; //объем текста на выходе
-typedef deque<string> prefix; // очередь префиксов
-map<prefix, vector<string>> statetab; // префикс-суффиксы
+const int NPREF = 2;
+const int MAXGEN = 1000;
+typedef std::deque<std::string> prefix;
+std::map<prefix, std::vector<std::string>> statetab;
 
-
-string read_file(string filename) {
-    ifstream input_file(filename.c_str());
+std::string read_file(std::string filename) {
+    std::ifstream input_file(filename.c_str());
     if (!input_file.is_open()) {
-        cout << "Error: could not open file \"" << filename << "\"" << endl;
+        std::cout << "Error: could not open file \""
+            << filename << "\"" << std::endl;
         exit(EXIT_FAILURE);
     }
-
-    string input_text = "";
-    string line;
+    std::string input_text = "";
+    std::string line;
     while (getline(input_file, line)) {
         input_text += line + "\n";
     }
     input_file.close();
     return input_text;
-
-
 }
 
-vector<string> split_text(string input_text) {
-    vector<string> words;
-    string word = "";
+std::vector<std::string> split_text(std::string input_text) {
+    std::vector<std::string> words;
+    std::string word = "";
     for (unsigned int i = 0; i < input_text.length(); i++) {
-        if (input_text[i] == ' ' || input_text[i] == '\n' || input_text[i] == '\r' || input_text[i] == '\t') {
+        if (input_text[i] == ' ' || input_text[i] == '\n' ||
+            input_text[i] == '\r' || input_text[i] == '\t') {
             if (word != "") {
                 words.push_back(word);
                 word = "";
@@ -53,13 +51,9 @@ vector<string> split_text(string input_text) {
     return words;
 }
 
-
-deque<prefix> Prefix(vector<string> words) {
-
-    deque <prefix> prefixes;
-
-
-    string word = "";
+std::deque<prefix> Prefix(std::vector<std::string> words) {
+    std::deque <prefix> prefixes;
+    std::string word = "";
     for (int i = 0; i < words.size() - NPREF; i++)
     {
         prefix temp;
@@ -67,16 +61,14 @@ deque<prefix> Prefix(vector<string> words) {
         {
             temp.push_back(words[j + i]);
         }
-
         prefixes.push_back(temp);
-
     }
     return prefixes;
 }
 
-vector<string> Suffix(prefix prefixes, vector<string> words) {
-
-    vector<string> suffix;
+std::vector<std::string> Suffix(prefix prefixes,
+    std::vector<std::string> words) {
+    std::vector<std::string> suffix;
     for (int i = 0; i < words.size() - NPREF; i++) {
         for (int j = 0; j < NPREF; j++) {
             if (prefixes[j] != words[i + j]) {
@@ -89,49 +81,47 @@ vector<string> Suffix(prefix prefixes, vector<string> words) {
     return suffix;
 }
 
-
-map<prefix, vector<string>> Prefix_Suffix(vector<string> words) {
-    deque<prefix> prefixes = Prefix(words);
+std::map<prefix, std::vector<std::string>> Prefix_Suffix
+(std::vector<std::string> words) {
+    std::deque<prefix> prefixes = Prefix(words);
 
     for (int i = 0; i < prefixes.size(); i++) {
-        map<prefix, vector<string>>::iterator it = statetab.find(prefixes[i]);
+        std::map<prefix, std::vector<std::string>>::iterator
+            it = statetab.find(prefixes[i]);
         if (it == statetab.end()) {
             statetab.insert({ prefixes[i],Suffix(prefixes[i],words) });
         }
         else {
-            vector<string> suf = it->second;
+            std::vector<std::string> suf = it->second;
             for (int i = 0; i < Suffix(prefixes[i], words).size(); i++) {
                 suf.push_back(Suffix(prefixes[i], words)[i]);
             }
         }
     }
-
     return statetab;
 }
 
 prefix GenerationPrefix() {
     int index = rand() % statetab.size();
-    deque<prefix> keys;
+    std::deque<prefix> keys;
     for (auto it = statetab.begin(); it != statetab.end(); it++) {
         keys.push_back(it->first);
-
     }
     prefix prefix_new = keys[index];
     return prefix_new;
 }
 
-string Generation_pref_suf(prefix prefix_new) {
-    string text = "";
+std::string Generation_pref_suf(prefix prefix_new) {
+    std::string text = "";
     srand(time(NULL));
-
-    vector<string> suffixes;
+    std::vector<std::string> suffixes;
     for (int i = 0; i < statetab.size(); i++) {
-        map<prefix, vector<string>>::iterator it = statetab.find(prefix_new);
+        std::map<prefix, std::vector<std::string>>::iterator
+            it = statetab.find(prefix_new);
         if (it != statetab.end()) {
             suffixes = it->second;
             break;
         }
-
     }
     if (suffixes.size() == 0) {
         return "";
@@ -140,29 +130,24 @@ string Generation_pref_suf(prefix prefix_new) {
         int index = rand() % suffixes.size();
         return suffixes[index];
     }
-
 }
 
-
-string Generation() {
-    string text = "";
+std::string Generation() {
+    std::string text = "";
     srand(time(NULL));
-
     prefix prefix_new = GenerationPrefix();
-
     for (int i = 0; i < prefix_new.size(); i++) {
         text = text + " " + prefix_new[i];
     }
-
     for (int j = 0; j < MAXGEN; j++) {
-        vector<string> suffixes;
+        std::vector<std::string> suffixes;
         for (int i = 0; i < statetab.size(); i++) {
-            map<prefix, vector<string>>::iterator it = statetab.find(prefix_new);
+            std::map<prefix, std::vector<std::string>>::iterator
+                it = statetab.find(prefix_new);
             if (it != statetab.end()) {
                 suffixes = it->second;
                 break;
             }
-
         }
         if (suffixes.size() == 0) {
             break;
