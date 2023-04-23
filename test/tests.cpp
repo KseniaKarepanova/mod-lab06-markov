503159e3646cfd471babdb1dac66f1049b714ca4
KseniaKarepanova <126484812+KseniaKarepanova@users.noreply.github.com>
2023-04-23 22:02:05 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,7 +10,6 @@
 typedef std::deque<std::string> prefix;
 
 TEST(test1, test_Prefix) {
-    setlocale(LC_ALL, "Russian");
     std::string text = read_file("file1.txt");
     std::vector<std::string> words = split_text(text);
     std::deque<prefix> prefixes = Prefix(words);
@@ -20,31 +19,31 @@ TEST(test1, test_Prefix) {
 }
 
 TEST(test2, test_Prefix_Suffix) {
-    setlocale(LC_ALL, "Russian");
     std::string text = read_file("file1.txt");
     std::vector<std::string> words = split_text(text);
     std::map<prefix, std::vector<std::string>>
         statetab = Prefix_Suffix(words);
     prefix new_prefix;
-    new_prefix.push_back("Куртка");
-    new_prefix.push_back("шофёра");
+    new_prefix.push_back("What");
+    new_prefix.push_back("is");
     std::string suf = Generation_pref_suf(new_prefix);
-    std::string suf_expected = "Пахнет";
+    std::string suf_expected = "this";
     EXPECT_EQ(suf, suf_expected);
 }
 
 TEST(test3, test_Prefix_Suffix2) {
-    setlocale(LC_ALL, "Russian");
-    std::string text = read_file("file2.txt");
+    std::string text = read_file("file1.txt");
     std::vector<std::string> words = split_text(text);
-    std::map<prefix, std::vector<std::string>>
+    std::map < prefix, std::vector < std::string>>
         statetab = Prefix_Suffix(words);
     prefix new_prefix;
-    new_prefix.push_back("Слово");
-    new_prefix.push_back("может");
+    new_prefix.push_back("time");
+    new_prefix.push_back("to");
     std::vector<std::string> suffixes;
-    suffixes.push_back("согреть,");
-    suffixes.push_back("нам");
+    suffixes.push_back("stand");
+    suffixes.push_back("see");
+    suffixes.push_back("turn");
+    suffixes.push_back("wait");
 
     std::string suf = Generation_pref_suf(new_prefix);
     bool flag = false;
@@ -56,10 +55,10 @@ TEST(test3, test_Prefix_Suffix2) {
 }
 
 TEST(test4, test_LengthText1) {
-    setlocale(LC_ALL, "Russian");
-    std::string text = read_file("file3.txt");
+    std::string text = read_file("file2.txt");
     std::vector<std::string> words = split_text(text);
-    Prefix_Suffix(words); // префикс-суффиксы
+    std::map<prefix, std::vector<std::string>>
+        statetab = Prefix_Suffix(words);
     std::string generate_text = Generation();
     std::vector<std::string> txt_split = split_text(generate_text);
     int expected_length = 3;
@@ -67,10 +66,10 @@ TEST(test4, test_LengthText1) {
 }
 
 TEST(test5, test_LengthText2) {
-    setlocale(LC_ALL, "Russian");
     std::string text = read_file("file1.txt");
     std::vector<std::string> words = split_text(text);
-    Prefix_Suffix(words);
+    std::map<prefix, std::vector<std::string>>
+        statetab = Prefix_Suffix(words);
     std::string generate_text = Generation();
     std::vector<std::string> txt_split = split_text(generate_text);
     int expected_length = 3;
